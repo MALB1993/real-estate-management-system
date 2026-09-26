215bc230d0b77993cf2f8bc0f90516f88cb844ba
Mostafa Alaeddin Barghamadi <MostafaAlaeddin.dev@gmail.com>
2026-09-26 11:26:44 +0330

    git commit -m "feat: set up main layout structure with navbar and footer partials"

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', __('messages.home'))
+
+@section('content')
+
+    
+
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,20 +5,21 @@
     <meta charset="UTF-8">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>
-        @yield('title', 'Real Estate Management System')
-    </title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="{{ __('messages.system_name') }}">
+    <meta name="keywords" content="{{ __('messages.system_name') }}">
+    <meta name="author" content="MALB1993">
+    <title>@yield('title', 'Real Estate Management System')</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="bg-gray-100 text-gray-900">
-
-    <main>
+    @include('partials.navbar')
+    <main class="container">
         @yield('content')
     </main>
-
+    @include('partials.footer')
 </body>
 
 </html>
